0fd031533d5097bb03b4e0598f8b9ca6eca03a42
Stargnite <sobayotoby@gmail.com>
2022-07-05 09:57:56 +0100

    Input/Output

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+def number_of_lines(filename=""):
+    """
+    Counts the number of lines from the filename
+
+    Arguments:
+        filename (str): The name of the file to count in
+    """
+    with open(filename, encoding='utf-8') as file:
+        lines = file.readlines()
+    return len(lines)
