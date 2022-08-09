637ee9acfd451c4aa9aad74d9c1e075867df95d5
Stargnite <sobayotoby@gmail.com>
2022-08-09 16:43:16 +0100

    updated changes

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- this is a script that displays the number of records in a table 
 -- this is a script that displays the number of records in  First_table of a  id=89
-SELECT COUNT(*) FROM First_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
