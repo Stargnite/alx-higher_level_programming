92275975fe071b7ad63444a574a2e033366f3278
Stargnite <sobayotoby@gmail.com>
2022-08-10 15:15:02 +0100

    added project files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- this is a script that creates a user of a server
 -- this is a script that creates user -- user_0d_1
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT '*'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
 ON *.*
-TO user_0d_1@localhost;
+TO 'user_0d_1'@'localhost';
