7673979c03c4bf63dc3269bd42fd6e0db2a89d54
Stargnite <sobayotoby@gmail.com>
2022-08-10 15:29:56 +0100

    added project files

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
--- this is a script that creates a database and a user if they don't exist
--- this is a script that creates hbtn_0d_2 and the user user_0d_2
+-- Create a database and user
+-- DDL query to create an user with specific privileges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+-- query privilegs for user_0d_2
+GRANT USAGE ON * . * TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2` . * TO 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
--- this is a script that lists all cities in a state
--- this is a script that  lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Carlifonia') ORDER BY cities_id ASC;
+-- List California's cities
+-- DML query to get all cities in Californi (state_id = 1)
+SELECT id,name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
