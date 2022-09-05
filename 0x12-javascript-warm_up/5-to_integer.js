af35250e72423e5ffbacf79d66203f143622b3b1
Sobayo <sobayotoby@gmail.com>
2022-09-05 16:28:51 +0100

    changed quotations

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argument = parseInt(process.argv[2]);
 if (argument) {
-  console.log("My number: " + argument);
+  console.log('My number: ' + argument);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
