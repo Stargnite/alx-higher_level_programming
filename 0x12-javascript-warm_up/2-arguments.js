c83b4eb9b7fcbf2720c5fe2208665cf6ca6e84b5
Sobayo <sobayotoby@gmail.com>
2022-09-05 16:13:19 +0100

    changed some variable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const theArgument = process.argv.length;
-	if (theArgument === 3 ) {
-		console.log('Argument found')
-	}
-	else if (theArgument > 3) {
-		console.log('Arguments found')
-	}
-	else {
-		console.log('No argument')
-	}
+if (theArgument === 3) {
+  console.log('Argument found');
+} else if (theArgument > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
